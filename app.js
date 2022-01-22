14cc9d03a70f97e439ed9ca106ce6a69b1b5dae4
Quangtruong1999 <Quangtruong61>
2022-01-22 20:23:05 +0700

    Init

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const {readFileSync} = require('fs')
+
 
 app.listen(process.env.PORT || 5000, () => console.log("Server running..."))
 
@@ -7,4 +9,9 @@ app.get('/', (request, response) => {
     response.send("Hello World!");
 })
 
+app.get('/users', (request, response) => {
+    let users = JSON.parse(readFileSync('users.json'));
+
+    response.send(users)
+})
 //heroku
