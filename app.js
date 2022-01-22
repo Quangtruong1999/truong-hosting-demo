0a673bd2fb55ebc19f9e5728d9b54d2e79c05929
hideonbush0512@gmail.com <hideonbush0512@gmail.com>
2022-01-23 01:59:23 +0700

    Init

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const app = express()
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan')
+const app = express();
+const {engine} = require('express-handlebars');
 const {readFileSync} = require('fs')
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'src/resources/views'));
+
+app.use(morgan('combined'));
 
 app.listen(process.env.PORT || 5000, () => console.log("Server running..."))
 
+// app.get('/', (request, response) => {
+//     response.send("Hello World!");
+// })
+
+
+
 app.get('/', (request, response) => {
-    response.send("Hello World!");
+    response.render("home");
 })
 
 app.get('/users', (request, response) => {
